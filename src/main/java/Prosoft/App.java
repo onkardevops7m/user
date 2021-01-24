b920f198bd0a232957d00ccce5a54c633802abd7
onkardevops7m <onkardevops7m@gmail.com>
2021-01-24 12:19:44 +0530

    first commit

---
+++ src/main/java/Prosoft/App.java
@@ -0,0 +1,13 @@
+package Prosoft;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
